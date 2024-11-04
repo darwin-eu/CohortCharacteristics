ffa2c3fe37f9c40f08e6c3acedf0ebf577d0c14f
catalamarti <marticatalasabate@gmail.com>
2024-11-04 14:47:58 +0100

    unique combinations

---
+++ tests/testthat/test-utilities.R
@@ -0,0 +1,26 @@
+test_that("getUniqueCombinationsSr", {
+  x <- dplyr::tibble(
+    cohort_name_reference = c("b", "y", "k"),
+    cohort_name_comparator = c("a", "x", "z")
+  ) |>
+    visOmopResults::uniteGroup(
+      cols = c("cohort_name_reference", "cohort_name_comparator")
+    )
+
+  # expect no change
+  expect_identical(x, getUniqueCombinationsSr(x))
+
+  # expect pairs drop
+  x <- dplyr::tibble(
+    cohort_name_reference = c("b", "y", "a"),
+    cohort_name_comparator = c("a", "x", "b")
+  ) |>
+    visOmopResults::uniteGroup(
+      cols = c("cohort_name_reference", "cohort_name_comparator")
+    )
+  expect_identical(
+    x |>
+      visOmopResults::filterGroup(.data$cohort_name_reference != "a"),
+    getUniqueCombinationsSr(x)
+  )
+})
